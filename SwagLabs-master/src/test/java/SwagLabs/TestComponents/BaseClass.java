d0a51eee973ce7414e6ad46b5b3a901e97e25194
Ram <ramprabhu2488@gmail.com>
2025-03-14 00:02:37 +0530

    Update BaseClass.java

--- SwagLabs-master/src/test/java/SwagLabs/TestComponents/BaseClass.java
+++ SwagLabs-master/src/test/java/SwagLabs/TestComponents/BaseClass.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Properties;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
